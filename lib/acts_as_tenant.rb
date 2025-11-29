d3b46ee71a5deecbbc4c3c250132b0aeaff36898
Thien Nguyen <thiennguyen@freec.asia>
2025-11-29 22:28:47 +0700

    just testing

--- lib/acts_as_tenant.rb
+++ lib/acts_as_tenant.rb
@@ -71,27 +71,27 @@ module ActsAsTenant
   end
 
   def self.current_tenant=(tenant)
-    Fiber.current.storage[:current_tenant] = tenant
+    Fiber.storage[:current_tenant] = tenant
   end
 
   def self.current_tenant
-    Fiber.current.storage[:current_tenant] || test_tenant || default_tenant
+    Fiber.storage[:current_tenant] || test_tenant || default_tenant
   end
 
   def self.test_tenant=(tenant)
-    Fiber.current.storage[:test_tenant] = tenant
+    Fiber.storage[:test_tenant] = tenant
   end
 
   def self.test_tenant
-    Fiber.current.storage[:test_tenant]
+    Fiber.storage[:test_tenant]
   end
 
   def self.unscoped=(unscoped)
-    Fiber.current.storage[:acts_as_tenant_unscoped] = unscoped
+    Fiber.storage[:acts_as_tenant_unscoped] = unscoped
   end
 
   def self.unscoped
-    Fiber.current.storage[:acts_as_tenant_unscoped]
+    Fiber.storage[:acts_as_tenant_unscoped]
   end
 
   def self.unscoped?
